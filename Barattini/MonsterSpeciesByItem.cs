c66d59d6da7298ad9acc6431e057a8eae5df5454
Luca Barattini <luca.barattini4@studio.unibo.it>
2022-05-05 12:22:03 +0200

    fixed warnings

--- Barattini/MonsterSpeciesByItem.cs
+++ Barattini/MonsterSpeciesByItem.cs
@@ -1,34 +1,32 @@
-﻿using Pokaiju.Guo.GameItem;
-using Pokaiju.Pierantoni;
+﻿namespace Pokaiju.Barattini;
 
-namespace Pokaiju.Barattini
+using Guo.GameItem;
+using Pierantoni;
+using Optional;
+
+public class MonsterSpeciesByItem : MonsterSpeciesSimple
 {
-    using Optional;
+    private readonly IGameItem _evolutionItem;
 
-    public class MonsterSpeciesByItem : MonsterSpeciesSimple
+    /// <summary>
+    /// MonsterSpeciesByItem constructor
+    /// </summary>
+    /// <param name="name">name</param>
+    /// <param name="info">info</param>
+    /// <param name="type">type</param>
+    /// <param name="stats">stats</param>
+    /// <param name="evolutionItem">evolutionItem</param>
+    /// <param name="evolution">evolution</param>
+    /// <param name="movesList">movesList</param>
+    public MonsterSpeciesByItem(string name, string info, MonsterType type, IMonsterStats stats,
+        IMonsterSpecies evolution, IGameItem evolutionItem, IList<IMoves> movesList)
+        : base(name, info, type, stats, Option.Some(evolution), EvolutionType.Item, movesList)
     {
-        private readonly IGameItem _evolutionItem;
+        _evolutionItem = evolutionItem;
+    }
 
-        /// <summary>
-        /// MonsterSpeciesByItem constructor
-        /// </summary>
-        /// <param name="name">name</param>
-        /// <param name="info">info</param>
-        /// <param name="type">type</param>
-        /// <param name="stats">stats</param>
-        /// <param name="evolutionItem">evolutionItem</param>
-        /// <param name="evolution">evolution</param>
-        /// <param name="movesList">movesList</param>
-        public MonsterSpeciesByItem(string name, string info, MonsterType type, IMonsterStats stats,
-            IMonsterSpecies evolution, IGameItem evolutionItem, IList<IMoves> movesList)
-            : base(name, info, type, stats, Option.Some(evolution), EvolutionType.Item , movesList)
-        {
-            _evolutionItem = evolutionItem;
-        }
-        
-        public IGameItem GetItem()
-        {
-            return _evolutionItem;
-        }
+    public IGameItem GetItem()
+    {
+        return _evolutionItem;
     }
 }
